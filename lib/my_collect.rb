8488832525c7864a939539efe4604972dae2c0ae
Harley McDowell <hmcdowell95@gmail.com>
2020-01-30 18:54:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  i = 0 
+  collection = []
+  while i < array.length
+  my_collect(array) do |name|
+    collection << yield(array[i]) 
+  end
+  i += 1 
+end
+collection 
+end
 
